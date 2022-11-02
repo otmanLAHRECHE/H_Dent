f0ae03502ce749d8aeb1d488d284771de902cbe5
SR-DG <salle.r.epsp@gmail.com>
2022-11-02 10:52:01 -0700

    venv problem

--- frontend/src/components/app_layout/Dashboard.js
+++ frontend/src/components/app_layout/Dashboard.js
@@ -1,8 +1,38 @@
 import * as React from 'react';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import VaccinesIcon from '@mui/icons-material/Vaccines';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PeopleIcon from '@mui/icons-material/People';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import LayersIcon from '@mui/icons-material/Layers';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import Groups3Icon from '@mui/icons-material/Groups3';
+import EventIcon from '@mui/icons-material/Event';
+import OutputIcon from '@mui/icons-material/Output';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+
+
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 
 
@@ -56,11 +86,119 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 const mdTheme = createTheme();
 
 function DashboardContent() {
+
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  const [page, setPage] = React.useState([true,false,false,false]);
+
+  const [toolBar,setToolBar] = React.useState("Tableau de bord")
   
   return (
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              {toolBar}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+                <ListItemButton selected = {page[0]} onClick={clickDashboard}>
+                  <ListItemIcon>
+                    <DashboardIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Tableau de bord"/>
+                </ListItemButton>
+                <ListItemButton selected={page[1]} onClick={clickSortie}>
+                  <ListItemIcon>
+                    <OutputIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Utilisateurs" />
+                </ListItemButton>
+                <ListItemButton selected={page[2]} onClick={clickCommande}>
+                  <ListItemIcon>
+                    <TextSnippetIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Medecines dentiste" />
+                </ListItemButton>
+                <ListItemButton selected={page[3]} onClick={clickStock}>
+                  <ListItemIcon>
+                    <InventoryIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Statestiques" />
+                </ListItemButton>
+             
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+        <Toolbar />
+
         
+        {page[0] ? <Statestiques/> : null}
+        {page[1] ? <Bon_sortie/> : null}
+        {page[2] ? <Bon_sortie_details/> : null}
+        {page[3] ? <Stock/> : null}
+        {page[4] ? <Medicaments/> : null}
+        {page[5] ? <Fournisseur/> : null}
+        {page[6] ? <Destinataire/> : null}
+        {page[7] ? <Expired_stock/> : null}
+        
+
+        
+          
+        </Box>
       </Box>
     </ThemeProvider>
   );
